6803d61883788c6d96fda6d558dc2e129085d792
Jtility <leejewon97@gmail.com>
2024-04-28 14:15:40 +0900

    Flexible을 적용한 레이아웃 생성 Column안에 Flexible 위젯을 child로 사용했다. weight값(flex)을 지정할 수 있다. Flexible의 child는 Container 혹은 Center로 Text와 Icon등을 감쌌다.     Text와 Icon은 영역을 본인들 크기만큼만 차지하는 특성을 가지기 때문.
    Text와 Icon의 색상을 Theme.of(context)에서 가져옴
    IconButton의 onPressed엔 일단 빈 함수를 넣어둠
    말고는 자잘한 text 크기나 weight 지정
    
    Column의 세번째 Flexible 속 Container가 가로로 넓게 지정되지 않은 듯(color 적용 범위)

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -12,6 +12,64 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+      body: Column(
+        children: [
+          Flexible(
+            flex: 1,
+            child: Container(
+              alignment: Alignment.bottomCenter,
+              child: Text(
+                '25:00',
+                style: TextStyle(
+                  color: Theme.of(context).cardColor,
+                  fontSize: 80.0,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ),
+          ),
+          Flexible(
+            flex: 2,
+            child: Center(
+              child: IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.play_circle_outline,
+                  size: 120.0,
+                  color: Theme.of(context).cardColor,
+                ),
+              ),
+            ),
+          ),
+          Flexible(
+            flex: 1,
+            child: Container(
+              color: Theme.of(context).cardColor,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'pomodoros',
+                    style: TextStyle(
+                      color: Theme.of(context).textTheme.displayLarge!.color,
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  Text(
+                    '0',
+                    style: TextStyle(
+                      color: Theme.of(context).textTheme.displayLarge!.color,
+                      fontSize: 60.0,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
